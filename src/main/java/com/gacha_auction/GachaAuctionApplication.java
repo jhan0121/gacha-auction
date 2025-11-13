4cd028b85e1c73701d0657761257c3260f01ce52
jhan0121 <jhan0121@gmail.com>
2025-11-14 01:42:06 +0900

    init: project init

---
+++ src/main/java/com/gacha_auction/GachaAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.gacha_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachaAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gacha_auction/TestGachaAuctionApplication.java
@@ -0,0 +1,11 @@
+package com.gacha_auction;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestGachaAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(GachaAuctionApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
