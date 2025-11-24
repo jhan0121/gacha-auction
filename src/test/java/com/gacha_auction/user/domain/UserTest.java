7c94b1b467575ef16c1a472643c23c6225f39189
jhan0121 <jhan0121@gmail.com>
2025-11-24 16:46:23 +0900

    test: UserTest 추가

---
+++ src/test/java/com/gacha_auction/user/domain/UserTest.java
@@ -0,0 +1,68 @@
+package com.gacha_auction.user.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.gacha_auction.exception.NotNullAllowedException;
+import java.util.stream.Stream;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+
+class UserTest {
+
+    @Test
+    @DisplayName("id가 없는 유저를 생성할 수 있다")
+    void createWithoutId() {
+        // given
+        final String name = "test";
+        final String password = "password";
+        final Long defaultCoinAmount = 1000L;
+
+        // when
+        final User actual = User.withoutId(name, password);
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(actual.getName()).isEqualTo(name);
+            softly.assertThat(actual.getPassword()).isEqualTo(password);
+            softly.assertThat(actual.getCoin()).isEqualTo(defaultCoinAmount);
+        });
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @DisplayName("name이 null인 유저를 생성 시, 예외를 발생한다")
+    void createWithNullName(String name) {
+        // given
+        final String password = "password";
+
+        // when
+        // then
+        assertThatThrownBy(() -> User.withoutId(name, password))
+                .isInstanceOf(NotNullAllowedException.class)
+                .hasMessageContaining("name must not null");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNullParameterCase")
+    @DisplayName("password가 null인 유저를 생성 시, 예외를 발생한다")
+    void createWithNullPassword(final String name, final String password, final String exceptionMessage) {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> User.withoutId(name, password))
+                .isInstanceOf(NotNullAllowedException.class)
+                .hasMessageContaining(exceptionMessage);
+    }
+
+    private static Stream<Arguments> provideNullParameterCase() {
+        return Stream.of(
+                Arguments.of(null, "password", "name must not null"),
+                Arguments.of("name", null, "password must not null")
+        );
+    }
+}
