43e5707d8088200b3caf2630a134cf60df2c3e94
jhan0121 <jhan0121@gmail.com>
2025-11-27 01:22:03 +0900

    fix: testcontainer 와 docker desktop 간 api 버전 차이로 인한 오류 수정

---
+++ src/test/java/com/gacha_auction/AbstractIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.gacha_auction;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:8.4")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpw");
+
+    @DynamicPropertySource
+    static void overrideProps(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+    }
+}
